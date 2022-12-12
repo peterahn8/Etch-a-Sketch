d10a82c85043af3cd2c9f0eca51c58b9ae563d80
Peter Ahn <peterahn8@gmail.com>
2022-12-12 16:21:40 -0500

    Complete reset and resize functionality

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const gridContainer = document.querySelector("#gridContainer");
+const resetButton = document.querySelector("#resetButton");
+const resizeButton = document.querySelector("#resizeButton");
+resetButton.addEventListener("click", () => reset());
+resizeButton.addEventListener("click", () => resize());
 
 function griddify(rows, columns) {
-
     for (let x = 1; x < rows+1; x++) {
         const newRow = document.createElement("div");
         gridContainer.appendChild(newRow);
-        newRow.setAttribute("id", `column${x}`);
+        newRow.setAttribute("id", "column");
 
         for (let y = 1;  y < columns+1; y++) {
             const newGridSquare = document.createElement("div");
@@ -14,15 +17,40 @@ function griddify(rows, columns) {
             newRow.appendChild(newGridSquare);
         }
     }
+
+    const gridSquares = document.querySelectorAll(".gridSquare")
+
+    gridSquares.forEach(square => {
+        square.addEventListener("mouseenter", function handleMouseOver(event) {
+            square.classList.add("gridSquareBlack");
+        });
+    });
 }
 
-griddify(16,16)
-
-const gridSquares = document.querySelectorAll(".gridSquare")
-
-gridSquares.forEach(square => {
-    square.addEventListener("mouseenter", function handleClick(event) {
-        console.log('mouseover', event);
-        square.setAttribute("style", "background-color: black;");
+function reset() {
+    const squareSelector = document.querySelectorAll(".gridSquare");
+    
+    squareSelector.forEach(square => {
+        square.classList.remove("gridSquareBlack");
     });
-});
+}
+
+function resize() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+
+    let gridX = parseInt(prompt("How many squares wide?"));
+    let gridY = parseInt(prompt("How many squares tall?"));
+    
+    if ((gridX >= 100) || (gridY >= 100)) {
+        gridX = parseInt(prompt("Please use values under 100 :("));
+        gridY = parseInt(prompt("Please use values under 100 :("));
+        griddify(gridX, gridY)
+    } else {
+        griddify(gridX, gridY);
+    }
+}
+
+// Set initial 16x16 grid
+griddify(16,16)
